d32ee48677302ed7d0d0ebf7340bb8761c6e9b39
Diederick Kaaij <diederickkaaij@gmail.com>
2019-10-17 14:50:15 +0200

    added look test

---
+++ features/look.feature
@@ -0,0 +1,6 @@
+Feature: Looking at our location
+
+    Scenario:
+        Given a player is in hal
+        When they type bekijk hal
+        Then they should get a description of hal
---
+++ features/steps/look_test.py
@@ -0,0 +1,15 @@
+from behave import given, when, then #pylint: disable=no-name-in-module
+from game import Game
+
+@given(u'a player is in hal')
+def test_move_hal(context):
+    context.game = Game.load("data/test_game.json")
+    context.game.move("hal")
+
+@when(u'they type bekijk hal')
+def test_bekijk(context):
+    context.description = context.game.describe("hal")
+
+@then(u'they should get a description of hal')
+def test_description(context):
+    assert context.description == "een ruimte die andere ruimtes met elkaar verbindt"
